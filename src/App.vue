2352505615e2e3621662fb582d24199cd3b11606
eunbinoh <nohonbi01@gmail.com>
2021-10-08 14:48:13 +0900

    input_storage complete

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,64 @@
+<template>
+  <header>
+      <h1> Todo by Vue.Ts</h1>
+  </header>
+  <main>
+      <TodoInput :item="content.todoText" @input="updateTodoText" @add="addTodoItem"/>
+      <div>
+          <ul>
+
+          </ul>
+      </div>
+  </main>
+</template>
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import TodoInput from "./components/TodoInput.vue"
+import { reactive } from 'vue';
+
+export interface Todo {
+  title: string,
+  done: boolean;
+}
+
+const content = reactive({
+  todoText:'',
+  todoItems: [] as Todo[]
+})
+
+const addTodoItem = () => {
+  const value = content.todoText;
+  const todo: Todo = {
+    title: value,
+    done: false
+  }
+
+  content.todoItems.push(todo);
+  storage.save(content.todoItems);
+  initTodoText();
+}
+
+const initTodoText = () => {
+  content.todoText ='';
+}
+
+const updateTodoText = (value: string) => {
+  if(typeof value === 'string'){
+    content.todoText = value;
+  }
+}
+const STORAGE_KEY = "vue-ts-storage1";
+const storage = {
+      save(TodoItem: Todo[]){
+        const parsed = JSON.stringify(TodoItem);
+        localStorage.setItem(STORAGE_KEY, parsed);
+      },
+      fetch(): Todo[]{
+        const todoItems = localStorage.getItem(STORAGE_KEY)||'[]';
+        return JSON.parse(todoItems);
+      }      
+}
+
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-</template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <label for="todo-input">오늘 업무 : </label>
+        <input id="todo-input" 
+                type="text"
+                :value="item"
+                @input="handleInput" 
+                @keypress.enter="addTodo"/>
+        <button @click="addTodo">추가</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'TodoInput',
+    props: {
+        item: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        handleInput(event: InputEvent) {
+            const eventTarget = event.target as HTMLInputElement;
+            this.$emit('input', eventTarget.value);
+        },
+        addTodo() {
+            this.$emit('add')
+        },
+    }
+})
+</script>
